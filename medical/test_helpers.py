e4c357318c5c4b47af5cf8188d97446d4f0131a8
delcroip <patrick.delcroix@swisstph.ch>
2024-08-24 23:14:35 +0200

    foolproof helper

--- medical/test_helpers.py
+++ medical/test_helpers.py
@@ -10,39 +10,58 @@ def get_item_of_type(item_type, valid=True):
 
 
 def create_test_service(category, valid=True, custom_props={}):
-    return Service.objects.create(
-        **{
-            "maximum_amount":5000,
-            "code": "TST-" + category,
-            "category": category,
-            "name": "Test service " + category,
-            "type": Service.TYPE_CURATIVE,
-            "level": 1,
-            "price": 100,
-            "patient_category": 15,
-            "care_type": Service.CARE_TYPE_OUT_PATIENT,
-            "validity_from": "2019-06-01",
-            "validity_to": None if valid else "2019-06-01",
-            "audit_user_id": -1,
-            **custom_props
-        }
-    )
+    custom_props = {k: v for k, v in custom_props.items() if hasattr(Service, k)} 
+    code = custom_props.pop('code', ('TST-' + category))     
+    obj = Service.objects.filter(code=code, validity_to__isnull=valid).first()
+    if obj is not None:
+        if custom_props:
+            Service.objects.filter(id=obj.id).update(**custom_props)
+            obj.refresh_from_db()
+        return obj
+    else:
+        return Service.objects.create(
+            **{
+                "maximum_amount": 5000,
+                "code": code,
+                "category": category,
+                "name": "Test service " + category,
+                "type": Service.TYPE_CURATIVE,
+                "level": 1,
+                "price": 100,
+                "patient_category": 15,
+                "care_type": Service.CARE_TYPE_OUT_PATIENT,
+                "validity_from": "2019-06-01",
+                "validity_to": None if valid else "2019-06-01",
+                "audit_user_id": -1,
+                **custom_props
+            }
+        )
 
 
-def create_test_item(item_type, valid=True, custom_props=None):
-    return Item.objects.create(
-        **{
-            "quantity":1,
-            "maximum_amount":225000,
-            "code": "XXX",
-            "type": item_type,
-            "name": "Test item",
-            "price": 100,
-            "patient_category": 15,
-            "care_type": 1,
-            "validity_from": "2019-06-01",
-            "validity_to": None if valid else "2019-06-01",
-            "audit_user_id": -1,
-            **(custom_props if custom_props else {})
-        }
-    )
+def create_test_item(item_type, valid=True, custom_props={}):
+    custom_props = {k: v for k, v in custom_props.items() if hasattr(Item, k)} 
+    code = custom_props.pop('code', 'XXX')
+        
+    obj = Item.objects.filter(code=code, validity_to__isnull=valid).first()
+    if obj is not None:
+        if custom_props:
+            Item.objects.filter(id=obj.id).update(**custom_props)
+            obj.refresh_from_db()
+        return obj
+    else:
+        return Item.objects.create(
+            **{
+                "quantity":1,
+                "maximum_amount":225000,
+                "code": "XXX",
+                "type": item_type,
+                "name": "Test item",
+                "price": 100,
+                "patient_category": 15,
+                "care_type": 1,
+                "validity_from": "2019-06-01",
+                "validity_to": None if valid else "2019-06-01",
+                "audit_user_id": -1,
+                **(custom_props if custom_props else {})
+            }
+        )
